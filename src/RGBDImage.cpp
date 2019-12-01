a8ab574917e99a7cfc55e4255b6c92263d53d5e9
Jack-long1996 <long_yw@126.com>
2019-12-01 16:18:51 +0800

    深度图像读入修改

--- src/RGBDImage.cpp
+++ src/RGBDImage.cpp
@@ -12,7 +12,7 @@ using namespace cv;
 RGBDImage::RGBDImage(string filename)
 {
     RGBImage = cv::imread(filename+"rgbimage0.png");
-    DepthImage = cv::imread(filename+"Depth_0.png");
+    DepthImage = cv::imread(filename+"Depth_0.png",IMREAD_ANYDEPTH);
 }
 
 RGBDImage::~RGBDImage()
